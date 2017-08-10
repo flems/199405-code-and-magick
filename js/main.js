77d5eb9237df6e051578ed05b0fec743020b48b8
Serg Borisov <borisov.s3@yandex.ru>
2017-08-10 17:58:30 +0400

    выполнил задания module1-task2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+var fireballSize = 22;
+var getFireballSpeed = function(left){
+  if(left){
+    return 5;
+  } else {
+    return 2;
+  }
+}
+var wizardSpeed = 3;
+var wizardWidth = 70;
+var getWizardHeight = function(){
+  return 1.337*wizardWidth
+}
+var getWizardX = function(width){
+  var magePositionX = width/2-wizardWidth/2;
+  return magePositionX;
+}
+var getWizardY = function(height){
+  var magePositionY = height/3;
+  return magePositionY;
+}
