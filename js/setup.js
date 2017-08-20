9d39b1a9e3332686320d656509477db92e0f4407
flems <borisov.s3@yandex.ru>
2017-08-20 18:08:39 +0400

    поправил код после проверки npm test

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,15 @@
-'use stict';
+'use strict';
 var userDialog = document.querySelector('.overlay');
 var otherUsers = userDialog.querySelector('.setup-similar-list');
 userDialog.classList.remove('hidden');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var  getRandomInt = function(min, max){
+var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
-const WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-const WIZARD_CLOAK = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-const WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+};
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_CLOAK = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var players = [
   {
     name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInt(0, WIZARD_NAMES.length)],
@@ -33,21 +33,21 @@ var players = [
   },
 
 ];
-var creatWizardElement = function(wizard){
+var creatWizardElement = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
-}
+};
 
-var fillWizardList = function(){
+var fillWizardList = function () {
   var WizardListFragment = document.createDocumentFragment();
-  for (var i = 0; i < 4; i++){
+  for (var i = 0; i < 4; i++) {
     WizardListFragment.appendChild(creatWizardElement(players[i]));
-  };
+  }
   otherUsers.appendChild(WizardListFragment);
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
-}
+};
 
 fillWizardList();
