a98f691b0288e69108e731d6bf8fd79059d767aa
Serg Borisov <borisov.s3@yandex.ru>
2017-08-21 12:44:53 +0400

    оптимизация кода

--- js/setup.js
+++ js/setup.js
@@ -4,16 +4,13 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_CLOAK = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-var players = [];
 
-var userDialog = document.querySelector('.overlay');
-var otherUsers = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-function fillPlayers(){
+function creatPlayers(){
+  var players = [];
   for (var i = 0; i < 4; i++){
     players[i] = {
       name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInt(0, WIZARD_NAMES.length)],
@@ -21,13 +18,12 @@ function fillPlayers(){
       eyesColor: WIZARD_EYES[getRandomInt(0, WIZARD_EYES.length)],
     }
   }
+  return players;
 }
-fillPlayers();
-
-userDialog.classList.remove('hidden');
+var players = creatPlayers();
 
 function creatWizardElement(wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var wizardElement = document.querySelector('#similar-wizard-template').content.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
@@ -35,11 +31,18 @@ function creatWizardElement(wizard) {
 };
 
 function fillWizardList() {
+  var otherUsers = document.querySelector('.setup-similar-list');
   var wizardListFragment = document.createDocumentFragment();
   for (var i = 0; i < 4; i++) {
     wizardListFragment.appendChild(creatWizardElement(players[i]));
   }
   otherUsers.appendChild(wizardListFragment);
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
 };
-fillWizardList();
+
+function loadWizard(){
+  var userDialog = document.querySelector('.overlay');
+  fillWizardList();
+  userDialog.classList.remove('hidden');
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+}
+loadWizard();
